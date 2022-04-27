c75fee5b156143b95bf9c5437b66d350f92c002a
yamodah <yassinedahlek@gmail.com>
2022-04-27 16:51:15 -0400

    moved classes to corresponding file and added script to index.html

--- index.js
+++ index.js
@@ -4,59 +4,6 @@ const ctx = canvas.getContext("2d");
 canvas.width = 1024;
 canvas.height = 576;
 const gravity = 0.7;
-class Player {
-  constructor(position, width, height, color) {
-    this.position = position;
-    this.width = width;
-    this.height = height;
-    this.color = color;
-    this.velocity = { x: 0, y: 0 };
-    this.lastKey;
-  }
-  draw() {
-    ctx.beginPath();
-    ctx.fillStyle = this.color;
-    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
-    ctx.fill();
-  }
-  update() {
-    this.draw();
-    this.position.x += this.velocity.x;
-    this.position.y += this.velocity.y;
-    if (this.position.y + this.height + this.velocity.y >= canvas.height) {
-      this.velocity.y = 0;
-      this.position.y = canvas.height - 150;
-    } else {
-      this.velocity.y += gravity;
-    }
-  }
-}
-class Ball {
-  constructor(position, radius, color, velocity) {
-    this.position = position;
-    this.radius = radius;
-    this.color = color;
-    this.velocity = velocity;
-  }
-  draw() {
-    ctx.beginPath();
-    ctx.arc(
-      this.position.x,
-      this.position.y,
-      this.radius,
-      0,
-      Math.PI * 2,
-      false
-    );
-    ctx.fillStyle = this.color;
-    ctx.fill();
-  }
-  update() {
-    this.draw();
-    this.x += this.velocity.x;
-    this.y += this.velocity.y;
-  }
-}
 
 const keys = {
   a: {
