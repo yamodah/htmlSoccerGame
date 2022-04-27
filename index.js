d2b2a2651888e6d5918a966ac3cfba8f84cfa474
yamodah <yassinedahlek@gmail.com>
2022-04-27 16:49:04 -0400

    implemented player movement

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ class Player {
     this.height = height;
     this.color = color;
     this.velocity = { x: 0, y: 0 };
+    this.lastKey;
   }
   draw() {
     ctx.beginPath();
@@ -30,34 +31,144 @@ class Player {
     }
   }
 }
-class Ball{
-    constructor(position, radius, color, velocity) {
-        this.position = position
-        this.radius = radius;
-        this.color = color;
-        this.velocity = velocity;
-      }
-      draw() {
-        ctx.beginPath();
-        ctx.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2, false);
-        ctx.fillStyle = this.color;
-        ctx.fill();
-      }
-      update() {
-        this.draw();
-        this.x += this.velocity.x;
-        this.y += this.velocity.y;
-      }
+class Ball {
+  constructor(position, radius, color, velocity) {
+    this.position = position;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+  }
+  draw() {
+    ctx.beginPath();
+    ctx.arc(
+      this.position.x,
+      this.position.y,
+      this.radius,
+      0,
+      Math.PI * 2,
+      false
+    );
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
+  update() {
+    this.draw();
+    this.x += this.velocity.x;
+    this.y += this.velocity.y;
+  }
 }
+
+const keys = {
+  a: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+  w: {
+    pressed: false,
+  },
+  ArrowLeft: {
+    pressed: false,
+  },
+  ArrowRight: {
+    pressed: false,
+  },
+  ArrowUp: {
+    pressed: false,
+  },
+};
+
 const player1 = new Player({ x: 0, y: 0 }, 75, 150, "blue");
 const player2 = new Player({ x: canvas.width - 75, y: 0 }, 75, 150, "red");
-const ball = new Ball({x:canvas.width/2, y:canvas.height -30}, 30, "white",{x:0,y:0})
+const ball = new Ball(
+  { x: canvas.width / 2, y: canvas.height - 30 },
+  30,
+  "white",
+  { x: 0, y: 0 }
+);
 function animate() {
-    requestAnimationFrame(animate);
-    ctx.fillStyle = "rgba(0,0,0)";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    player1.update();
-    player2.update();
-    ball.update()
+  requestAnimationFrame(animate);
+  ctx.fillStyle = "rgba(0,0,0)";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  player1.update();
+  player2.update();
+  ball.update();
+
+  if (keys.a.pressed && player1.lastKey === "a" && player1.position.x >= 0) {
+    player1.velocity.x = -5;
+  } else if (keys.d.pressed && player1.lastKey === "d") {
+    player1.velocity.x = 5;
+  } else {
+    player1.velocity.x = 0;
+  }
+  if (
+    keys.ArrowLeft.pressed &&
+    player2.lastKey === "ArrowLeft" &&
+    player2.position.x >= 0
+  ) {
+    player2.velocity.x = -5;
+  } else if (keys.ArrowRight.pressed && player2.lastKey === "ArrowRight") {
+    player2.velocity.x = 5;
+  } else {
+    player2.velocity.x = 0;
+  }
 }
-animate()
+animate();
+
+window.addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case "d":
+      keys.d.pressed = true;
+      player1.lastKey = "d";
+      break;
+    case "a":
+      keys.a.pressed = true;
+      player1.lastKey = "a";
+      break;
+    case "w":
+      if (player1.position.y > canvas.height / 2) {
+        player1.velocity.y = -20;
+      }
+      break;
+  }
+  switch (event.key) {
+    case "ArrowRight":
+      keys.ArrowRight.pressed = true;
+      player2.lastKey = "ArrowRight";
+      break;
+    case "ArrowLeft":
+      keys.ArrowLeft.pressed = true;
+      player2.lastKey = "ArrowLeft";
+      break;
+    case "ArrowUp":
+      if (player2.position.y > canvas.height / 2) {
+        player2.velocity.y = -20;
+      }
+      break;
+  }
+});
+window.addEventListener("keyup", (event) => {
+  switch (event.key) {
+    case "d":
+      keys.d.pressed = false;
+      break;
+    case "a":
+      keys.a.pressed = false;
+      break;
+    case "w":
+      keys.w.pressed = false;
+      break;
+    case "ArrowLeft":
+      keys.ArrowLeft.pressed = false;
+      break;
+    case "ArrowRight":
+      keys.ArrowRight.pressed = false;
+      break;
+    case "ArrowUp":
+      keys.ArrowUp.pressed = false;
+      break;
+    default:
+  }
+  console.log(event);
+});
