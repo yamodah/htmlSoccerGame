30d262dfafefd39cceb56d52efa048b2fb2d172a
yamodah <yassinedahlek@gmail.com>
2022-04-28 00:04:02 -0400

    added rectangleCollison function

--- js/utils.js
+++ js/utils.js
@@ -12,3 +12,14 @@ function rectangleCircleCollison({circle, rectangle}){
     var dy=distY-rectangle.height/2;
     return (dx*dx+dy*dy<=(circle.radius*circle.radius));
 }
+function rectangularCollision({ rectangle1, rectangle2 }) {
+    return (
+      rectangle1.attackBox.position.x + rectangle1.attackBox.width >=
+        rectangle2.position.x &&
+      rectangle1.attackBox.position.x <=
+        rectangle2.position.x + rectangle2.width &&
+      rectangle1.attackBox.position.y + rectangle1.attackBox.height >=
+        rectangle2.position.y &&
+      rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
+    );
+  }
