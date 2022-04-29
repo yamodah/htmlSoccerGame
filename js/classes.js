6e633e8cc4478ac008000e17636c89f9fa3b8b46
yamodah <yassinedahlek@gmail.com>
2022-04-29 10:15:22 -0400

    added sprite class

--- js/classes.js
+++ js/classes.js
@@ -1,3 +1,51 @@
+class Sprite {
+  constructor({
+    position,
+    imgSrc,
+    scale = 1,
+    framesMax = 1,
+    offset = { x: 0, y: 0 },
+  }) {
+    this.position = position;
+    this.height = 150;
+    this.width = 50;
+    this.image = new Image();
+    this.image.src = `${imgSrc}`;
+    this.scale = scale;
+    this.framesMax = framesMax;
+    this.framesCurrent = 0;
+    this.framesElapsed = 0;
+    this.framesHold = 5;
+    this.offset = offset;
+  }
+  draw() {
+    ctx.drawImage(
+      this.image,
+      this.framesCurrent * (this.image.width / this.framesMax),
+      0,
+      this.image.width / this.framesMax,
+      this.image.height,
+      this.position.x - this.offset.x,
+      this.position.y - this.offset.y,
+      (this.image.width / this.framesMax) * this.scale,
+      this.image.height * this.scale
+    );
+  }
+  update() {
+    this.draw();
+    this.animateFrames();
+  }
+  animateFrames() {
+    this.framesElapsed++;
+    if (this.framesElapsed % this.framesHold === 0) {
+      if (this.framesCurrent < this.framesMax - 1) {
+        this.framesCurrent++;
+      } else {
+        this.framesCurrent = 0;
+      }
+    }
+  }
+}
 class Player {
   constructor(position, width, height, color) {
     this.position = position;
